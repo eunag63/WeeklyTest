4d2e8044766eae6bed4fd511e062d221ee2a38d7
eunag63 <77442567+eunag63@users.noreply.github.com>
2021-10-01 18:46:54 +0900

    키워드 - 기능, 단위 #issue번호
    변경사항 :

--- test/1001/app.py
+++ test/1001/app.py
@@ -1,42 +1,50 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import datetime
 
 app = Flask(__name__)
 
-import requests
-
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
+client = MongoClient("mongodb://localhost:27017/")
+db = client.dbStock
 
 
-## HTML을 주는 부분
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 
-@app.route('/cho', methods=['GET'])
-def listing():
-    choises = db.choise.distint("group")
+@app.route('/post', methods=['POST'])
+def save_post():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+    print(title_receive, content_receive)
 
-    return jsonify({'each_chosies': choises})
+    for i in range(1, 100):
+        doc = {
+            'idx': i,
+            'title': title_receive,
+            'content': content_receive,
+            'reg_date': datetime.datetime.utcnow(),
+        }
 
+        db.articles.insert_one(doc)
 
-## API 역할을 하는 부분
-@app.route('/cho', methods=['POST'])
-def write_review():
-    first_receive = request.form['first_give']
-    second_receive = request.form['second_give']
-
-    doc = {
-        'first': first_receive,
-        'second': second_receive,
-    }
-    db.choise.insert_one(doc)
-
-    return jsonify({'msg': '저장완료'})
+    return jsonify({'msg':'저장이 완료되었습니다.'})
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+@app.route('/post', methods=['GET'])
+def get_post():
+    articles = list(db.articles.find({}, {'_id': False}).sort('reg_date', 1))
+    return jsonify({'all_articles':articles})
+
+
+@app.route('/post', methods=['DELETE'])
+def delete_post():
+    idx_receive = request.form['idx_give']
+    db.articles.delete_one({"idx": idx_receive})
+
+    return jsonify({'msg':'삭제가 완료되었습니다.'})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
